1a76dab5bfd1be3a4a29361385a9f0bd35cd66c8
jadbassil <31135113+jadbassil@users.noreply.github.com>
2026-01-16 09:28:03 +0100

    chore: update deployment adapter from Vercel to Netlify in package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import vercel from '@astrojs/vercel';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +11,5 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  adapter: vercel()
+  adapter: netlify()
 });
